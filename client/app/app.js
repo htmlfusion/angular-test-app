3f1af6a240731334b5bd627934314ee9da6385db
Jesse Vander Does <jesse@htmlfusion.com>
2015-03-30 08:10:40 -0700

    First commit

---
+++ client/app/app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+angular.module('testProjApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+    $httpProvider.interceptors.push('authInterceptor');
+  })
+
+  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
+    return {
+      // Add authorization token to headers
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($cookieStore.get('token')) {
+          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+        }
+        return config;
+      },
+
+      // Intercept 401s and redirect you to login
+      responseError: function(response) {
+        if(response.status === 401) {
+          $location.path('/login');
+          // remove any stale tokens
+          $cookieStore.remove('token');
+          return $q.reject(response);
+        }
+        else {
+          return $q.reject(response);
+        }
+      }
+    };
+  })
+  
+  .factory('nativeHttpInterceptor', function (device) {
+    return {
+      //If its an api request and we're in a native context convert it to an absolute url
+      request: function (config) {
+        var internalURL = config.url.indexOf('/api/')>-1;
+        if(internalURL && device.isNative()){
+          config.url = device.apiRoot+config.url;
+        }
+        return config;
+      }
+    };
+  })
+  .config(function($httpProvider){
+    $httpProvider.interceptors.push('nativeHttpInterceptor');
+  })
+  .run(function ($rootScope, $location, Auth) {
+    // Redirect to login if route requires auth and you're not logged in
+    $rootScope.$on('$stateChangeStart', function (event, next) {
+      Auth.isLoggedInAsync(function(loggedIn) {
+        if (next.authenticate && !loggedIn) {
+          $location.path('/login');
+        }
+      });
+    });
+  });
